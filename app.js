736af87a0d76a7c4e82204042e659ef1ce5af6af
Brandon <brandonsamuel72@gmail.com>
2019-05-30 16:23:47 -0700

    create login/register routes. style login/register pages

--- app.js
+++ app.js
@@ -5,8 +5,18 @@ const mongoose = require('mongoose');
 
 const app = express();
 
+app.use(express.static("public"))
 app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+app.get('/register', (req, res) => {
+    res.render('register');
+});
 
 
 app.listen(8080, (req, res) => {
