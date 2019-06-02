dd25780a32ec22ec179eba70615d9b06c17d3a7a
Brandon <brandonsamuel72@gmail.com>
2019-06-01 17:12:24 -0700

    user can register with username, email and password

--- app.js
+++ app.js
@@ -2,34 +2,57 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const ejs = require('ejs');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
 
 const app = express();
 
-app.use(express.static("public"))
+app.use(express.static("public"));
 app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(session({
+    secret: 'keyboard cat',
+    resave: true,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Connecting to MongoDB
 mongoose.connect('mongodb://localhost:27017/social-loginDB', {useNewUrlParser: true})
 .then(() => console.log('MongoDB Connected...'))
 .catch(() => console.log('Error connecting to MongoDB...'));
+mongoose.set('useCreateIndex', true);
 
 
 // Creating DB Schema
-const user = new mongoose.Schema({
-    firstName: {
-        type: String,
-        required: true
-    },
-    lastName: {
-        type: String,
-        required: true,
-    },
-    email: {
-        type: String,
-        required: true
-    }
+const userSchema = new mongoose.Schema({
+    username: String,
+    email: String,
+    password: String
 });
+
+
+userSchema.plugin(passportLocalMongoose);
+
+const User = mongoose.model('User', userSchema);
+
+passport.use(User.createStrategy());
+ 
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+  
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+        done(err, user);
+    });
+});
+
+
 
 app.get('/login', (req, res) => {
     res.render('login');
@@ -37,6 +60,20 @@ app.get('/login', (req, res) => {
 
 app.get('/register', (req, res) => {
     res.render('register');
+});
+
+
+app.post('/register', (req, res) => {
+    User.register({username: req.body.username, email: req.body.email}, req.body.password, function(err, user) {
+        if(err) {
+            console.log('Error registering user....', err)
+            res.redirect('/register');
+        } else {
+            passport.authenticate('local')(req, res, function() {
+                res.redirect('/success');
+            });
+        }
+    });
 });
 
 
